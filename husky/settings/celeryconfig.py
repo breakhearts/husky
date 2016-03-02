8fd5d4b80c53288ec45743c9a6bba0b9e6b53b53
breakhearts <lxx1234@gmail.com>
2016-03-02 13:17:34 +0800

    =missing schedule args

--- husky/settings/celeryconfig.py
+++ husky/settings/celeryconfig.py
@@ -9,11 +9,13 @@ CELERY_DISABLE_RATE_LIMITS = True
 BROKER_TRANSPORT_OPTIONS = {'fanout_prefix': True ,'fanout_patterns': True, 'visibility_timeout': 43200}
 
 from celery.schedules import crontab
+from husky.api import nasdaq
 CELERY_TIMEZONE = 'US/Eastern'
 CELERYBEAT_SCHEDULE = {
     # Executes every Monday morning at 7:30 A.M
     'crawl-nasdaq-stock-time-quote': {
         'task': 'husky.tasks.stock_tasks.crawl_nasdaq_stock_quote_batch',
-        'schedule': crontab(hour=17, minute=0, day_of_week = '1-5')
+        'schedule': crontab(hour=17, minute=0, day_of_week = '1-5'),
+        'args' : nasdaq.REAL_TIME_QUOTE
     },
 }
